d60cf1653762c56d8fd9bf917e6cffebae9a9d8c
Raro Macias <ggmacias@gmail.com>
2022-10-26 13:45:47 -0400

    detail

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const DetailScreen = () => {  
+  const {id} = useParams()
+  const [recipe, setRecipes] = useState([])
+    const url = 'https://recipes.devmountain.com'
+
+    useEffect(()=> {
+      axios.get(`${url}/recipes/${id}`)
+      .then(res=> setRecipes(res.data))
+  }, [])
+
   return (
     <section>
-     
+        {recipe.recipe_name}
      </section>
   );
 };
